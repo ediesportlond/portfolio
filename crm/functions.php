fcec88912ccd39854484984268795c8ace9746f7
Eddie Espinosa <espinosa.edilson@gmail.com>
2022-12-11 16:50:31 -0500

    pushing functions

--- crm/functions.php
+++ crm/functions.php
@@ -2,10 +2,10 @@
     /*checkOTP and changePass contain site URL
     Needs to be updated when site is migrated.*/
 
-    $servername = "localhost";
-    $username = "itseddie_admin";
-    $password = ".)sgw%;jJPIp";
-    $dbname = "itseddie_main";
+    $servername = "";
+    $username = "";
+    $password = "";
+    $dbname = "";
 
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
